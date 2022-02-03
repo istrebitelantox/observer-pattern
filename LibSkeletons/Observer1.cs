c3a54e73843a8d5838085d7029bfb4e9471f1edd
istrebitelantox <geekpook@gmail.com>
2022-02-03 09:21:17 +0300

    Добавьте файлы проекта.

---
+++ LibSkeletons/Observer1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibSkeletons
+{
+    public class Observer1
+    {
+        public int perechis = 0;
+        public void Update()
+        {
+            
+        }
+    }
+}
